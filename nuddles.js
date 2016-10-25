0c26c3d29b46bfcb93d188801d6d50fa66d3c7f0
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-25 20:22:41 +0200

    Remove else check

--- nuddles.js
+++ nuddles.js
@@ -3,13 +3,12 @@ const Nuddles = function (credentials) {
         throw new Error("You have to pass an object")
     }
 
-    let credentials_keys = Object.keys(credentials)
-    if ( !credentials_keys.includes("clientId") || !credentials_keys.includes("clientSecret") ){
-        throw new Error("The config object takes a clientId and a clientSecret")
-    } else {
-        this.clientId       = credentials.clientId
-        this.clientSecret   = credentials.clientSecret
-    }
+    if (!credentials.clientId || !credentials.clientSecret){
+        throw new Error("Missing required fields. Please refer to the documentation.")
+    } 
+
+    this.clientId       = credentials.clientId
+    this.clientSecret   = credentials.clientSecret
     this.url = "http://api.foursquare.com"
     this.version = "20161024"
 
