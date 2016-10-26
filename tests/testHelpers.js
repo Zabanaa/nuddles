3ea66de3d694ad3607df734bcb385d4f53470f7d
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-26 17:40:18 +0200

    Add describe blocks

--- tests/testHelpers.js
+++ tests/testHelpers.js
@@ -1,8 +1,25 @@
 const chai      = require('chai')
 const chaiHttp  = require('chai-http')
+const assert    = chai.assert
 
 // test object required
+describe('Object required', () => {
+   
+    // test with an object
+    // test without an object 
+
+})
+
 // test urlSerialise 
+describe('Serialise Query Parameters Object', () => {
+    // test that it works
+    // test that it fails if no object is passed
+})
+
 // test makeRequest
+describe('Make calls to API', () => {
+
+
+})
 
 
