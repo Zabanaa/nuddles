1175cd267ed6428350413b07d505c5a8a21cce59
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-27 11:41:24 +0200

    create tests for urlSerialise method and fix naming issue on objectRequired helper

--- tests/testHelpers.js
+++ tests/testHelpers.js
@@ -1,27 +1,30 @@
 const chai      = require('chai')
-const chaiHttp  = require('chai-http')
 const assert    = chai.assert
 const helpers   = require('../helpers')
 
 describe('Object required', () => {
     
     it('throws an error if the argument passed is not an object', () => {
-        assert.throws( function(){helpers.objectRequired('hello')}, Error, "An object must be passed")
+        assert.throws( function(){helpers.checkIsObject('hello')}, Error, "An object must be passed")
     })
 
     it('does not throw an error if the argument passed is an object', () => {
-        assert.doesNotThrow( function(){ helpers.objectRequired({name: "karim", city: "paris"})}, Error)
+        assert.doesNotThrow( function(){ helpers.checkIsObject({name: "karim", city: "paris"})}, Error)
     })
 })
 
 describe('Serialise Query Parameters Object', () => {
-    // assertEqual
-    // assertThrows if no object is passed 
+
+    let obj     = {key1: "key_1", key2: "key_2"}
+    let result  = "key1=key_1&key2=key_2&v=20161026"
+    
+    it('serialises an object to a URL safe string', () => {
+        assert.equal( helpers.urlSerialise(obj), result )
+    })
+
+    it('throws an error if no object is passed', () => {
+        assert.throws( function(){ helpers.urlSerialise("kejre") }, Error)
+    })
+
+
 })
-
-describe('Make calls to API', () => {
-
-
-})
-
-
