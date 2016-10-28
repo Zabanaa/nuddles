ab8fff3d1f0fa931024718bc061c3280f30ce9c9
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-28 09:47:47 +0200

    Refactor api test

--- tests/testApi.js
+++ tests/testApi.js
@@ -8,17 +8,17 @@ const  credentials  = require('../credentials')
 describe("Calls to the API actually work", (done) => {
 
     it('returns a list of categories', () => {
-        
+
         const nuddles = new Nuddles({
             clientId: credentials.clientId,
             clientSecret: credentials.clientSecret
-        }) 
-        
-        let categories = nuddles.callApi('venues/categories', {}) 
+        })
+
+        let categories = nuddles.callApi('/venues/categories', {})
         return categories.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'categories')
-            assert.isArray(data.response.categories) 
+            assert.isArray(data.response.categories)
             assert.isAtLeast(data.response.categories.length, 1, 'The categories array is at least equal to 1')
 
         })
