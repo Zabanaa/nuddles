13ad3501bde1cf3f301b0850e553462be1f0e237
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-28 11:02:46 +0200

    refactor error message in reject call within makeRequest

--- helpers.js
+++ helpers.js
@@ -7,36 +7,34 @@ const checkIsObject = (arg) => {
 }
 
 const urlSerialise = (queryParams) => {
-    
-    checkIsObject(queryParams) 
 
-    let queryString = [] 
+    checkIsObject(queryParams)
+
+    let queryString = []
 
     for(let key in queryParams) {
         queryString.push( key + "=" + queryParams[key] )
     }
 
     queryString.splice(queryString.length, 0, "v=20161026")
-    
+
     return queryString.join('&')
 }
 
 const makeRequest = (options) => {
-   
+
     return new Promise( (resolve, reject) => {
-       
+
         nets(options, (err, response, body) => {
 
+            let responseBody = JSON.parse(body)
+
             if (response.statusCode >= 200 && response.statusCode <= 299) {
-                
-                resolve(JSON.parse(body))
-
+                resolve(responseBody)
             } else {
-                
-                reject("Error! The server responded with a status code of " + response.statusCode + " and sent the following message: " + response.statusMessage)
 
+                reject("Error! The server responded with a status code of " + responseBody.meta.code + " and sent the following message: " + responseBody.meta.errorDetail)
             }
-
         })
     })
 }
