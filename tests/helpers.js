0c2afea40ca47ff2dd02dd6e52b9b9aa52f31b19
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-29 15:19:31 +0200

    remove empty object test on urlSerialise() method

--- tests/helpers.js
+++ tests/helpers.js
@@ -3,7 +3,7 @@ const assert    = chai.assert
 const helpers   = require('../helpers')
 
 describe('Object required', () => {
-    
+
     it('throws an error if the argument passed is not an object', () => {
         assert.throws( function(){helpers.checkIsObject('hello')}, Error, "An object must be passed")
     })
@@ -17,12 +17,9 @@ describe('Serialise Query Parameters Object', () => {
 
     let obj     = {key1: "key_1", key2: "key_2"}
     let result  = "key1=key_1&key2=key_2&v=20161026"
-    
+
     it('serialises an object to a URL safe string', () => {
         assert.equal( helpers.urlSerialise(obj), result )
     })
 
-    it('throws an error if no object is passed', () => {
-        assert.throws( function(){ helpers.urlSerialise("kejre") }, Error)
-    })
 })
