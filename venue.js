cb77cb2c15485e26966cabc55a69ded3f404d8e4
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-30 16:52:09 +0100

    Create venue class

---
+++ venue.js
@@ -0,0 +1,50 @@
+const Nuddles = require('./nuddles')
+
+const Venue = function(venueId) {
+    // Make super call
+    this.venueId = venueId
+}
+
+// Do the inheritance shit here
+
+Venue.prototype.getDetails = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getPhotos = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getEvents = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getLikes = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getNextVenues = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getOpeningHours = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getThirdPartyLinks = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getMenu = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getTips = function() {
+    // return this.callApi
+}
+
+Venue.prototype.getLists = function() {
+    // return this.callApi
+}
+
+module.exports = Venue
