bed2ca72144fb5ea33252ecc2ac1eeefe83a8725
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-31 13:53:52 +0100

    Create List.getDetails() method

--- list.js
+++ list.js
@@ -3,9 +3,14 @@ const Nuddles = require('./nuddles')
 const List = function(nuddlesObject, listId) {
     Nuddles.call(this, nuddlesObject)
     this.listId = listId
+    this.rootPath = '/lists/' + this.listId
 }
 
 List.prototype = Object.create(Nuddles.prototype)
 List.prototype.constructor = List
+
+List.prototype.getDetails = function() {
+    return this.callApi(this.rootPath)
+}
 
 module.exports = List
