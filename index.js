920bcb699d5765fc180243412f7183116e81f46e
MilkshakePanda <iam.superpeluche@gmail.com>
2016-10-31 20:32:31 +0100

    Ignore app.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const Nuddles       = require('./nuddles')
-const credentials   = require('./credentials')
-
-const nuddles = new Nuddles({
-    clientId: credentials.clientId, 
-    clientSecret: credentials.clientSecret 
-})
