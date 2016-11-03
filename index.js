18d36b707017546aa5bf1572d910a26ce116d0b5
MilkshakePanda <iam.superpeluche@gmail.com>
2016-11-03 11:15:21 +0100

    create main entry point file

---
+++ index.js
@@ -0,0 +1,9 @@
+const Client  = require('./client')
+const Venue   = require('./venue')
+const List    = require('./list')
+
+module.exports = {
+    Client: Client,
+    Venue: Venue,
+    List: List
+}
