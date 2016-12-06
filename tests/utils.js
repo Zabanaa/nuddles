0519a70be2381011d341efe510521f4eda1bbd23
MilkshakePanda <iam.superpeluche@gmail.com>
2016-12-06 15:52:41 +0100

    rename utils.urlSerialise to serialise

--- tests/utils.js
+++ tests/utils.js
@@ -2,24 +2,13 @@ const chai      = require('chai')
 const assert    = chai.assert
 const utils     = require('../utils')
 
-describe('Test utils.checkIsObject()', () => {
-
-    it('throws an error if the argument passed is not an object', () => {
-        assert.throws( function(){utils.checkIsObject('hello')}, Error, "An object must be passed")
-    })
-
-    it('does not throw an error if the argument passed is an object', () => {
-        assert.doesNotThrow( function(){ utils.checkIsObject({name: "karim", city: "paris"})}, Error)
-    })
-})
-
-describe('Test utils.urlSerialise()', () => {
+describe('Test utils.serialise()', () => {
 
     let obj     = {key1: "key_1", key2: "key_2"}
     let result  = "key1=key_1&key2=key_2"
 
     it('serialises an object to a URL safe string', () => {
-        assert.equal( utils.urlSerialise(obj), result )
+        assert.equal( utils.serialise(obj), result )
     })
 
 })
