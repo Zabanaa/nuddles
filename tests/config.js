9463d2de67381a63b6a1863c2a7327ee89a73f8b
MilkshakePanda <iam.superpeluche@gmail.com>
2016-12-12 12:27:31 +0100

    fix crendtials import issues

--- tests/config.js
+++ tests/config.js
@@ -2,19 +2,17 @@ let clientId, clientSecret, accessToken
 const Nuddles           = require('../index').Client
 
 try{
-    const credentials       = { clientId, clientSecret, accessToken } =  require('../config/credentials')
+    const credentials                   = { clientId, clientSecret, accessToken } =  require('../config/credentials')
+    module.exports.userlessClient       = new Nuddles({ clientId, clientSecret })
+    module.exports.authClient           = new Nuddles({ accessToken })
+
 }
 catch(e) {
     console.log("Config file not found. Using environment variables instead.")
     clientId            = process.env['CLIENT_ID']
     clientSecret        = process.env['CLIENT_SECRET']
     accessToken         = process.env['ACCESS_TOKEN']
+
+    module.exports.userlessClient = new Nuddles({ clientId, clientSecret })
+    module.exports.authClient     = new Nuddles({ clientId, clientSecret })
 }
-
-let userlessClient      = new Nuddles({ clientId, clientSecret })
-let authClient          = new Nuddles({ accessToken })
-
-console.log(userlessClient)
-console.log(authClient)
-
-module.exports = { userlessClient, authClient }
