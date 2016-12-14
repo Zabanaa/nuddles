639fdd802374dfe744c2c91e13dcb8e97ee0a418
MilkshakePanda <iam.superpeluche@gmail.com>
2016-12-14 16:30:44 +0100

    add tip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports.Client   = require('./lib/nuddles')
+module.exports.Tip      = require('./lib/tip')
 module.exports.Venue    = require('./lib/venue')
 module.exports.List     = require('./lib/list')
 module.exports.Checkin  = require('./lib/checkin')
