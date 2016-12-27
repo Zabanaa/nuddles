d164b721349ca7a62945dc581e2ca17ad1071051
MilkshakePanda <iam.superpeluche@gmail.com>
2016-12-27 14:59:25 +0100

    move venue related methods from Client to Venue

--- tests/userless/nuddles.js
+++ tests/userless/nuddles.js
@@ -76,22 +76,6 @@ describe("Test Nuddles.generateOAuthTokenURL method", () => {
 
 })
 
-describe('Test Nuddles.searchVenues', () => {
-
-    it('searches venues and returns results based on location',() => {
-
-        let params          = {'near': 'Paris, France'}
-        let searchVenues    = client.searchVenues(params)
-        return searchVenues.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'venues')
-            assert.isArray(response.venues)
-            assert.include(response.geocode.where, 'paris france')
-        })
-    })
-})
-
 describe('Test Nuddles.getChekinsLikes', () => {
 
     it('returns a list of likes for a specific checkin', () => {
@@ -150,103 +134,6 @@ describe('Test Nuddles tips methods', () => {
             assert.isArray(response.lists.groups)
         })
     })
-})
-
-describe('Test Nuddles.getVenueDetail', (done) => {
-
-    it('returns all the details for a specific venue given an ID', () => {
-        let venueId      = '4adcda0af964a520623421e3'
-        let venueDetails = client.getVenueDetail(venueId)
-
-        return venueDetails.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'venue')
-            assert.isObject(response.venue, "venue is indeed of type object")
-        })
-    })
-
-    it('returns a 400 if the provided venueID is invalid', () => {
-        let venueId = "invalididbruv"
-        let venueDetails = client.getVenueDetail(venueId)
-        return venueDetails.catch( (errorMsg) => {
-            assert.include(errorMsg, '400')
-            assert.include(errorMsg, venueId + ' is invalid for venue id')
-        })
-    })
-
-})
-
-describe(' Test Nuddles.getVenueCategories', (done) => {
-
-    it('returns a list of all categories', () => {
-        let venueCategories = client.getVenueCategories()
-        return venueCategories.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'categories')
-            assert.isArray(response.categories)
-            assert.isAtLeast(response.categories.length, 1)
-        })
-    })
-
-})
-
-describe('Test Nuddles.getTrendingVenues', (done) => {
-
-    it('returns a list of trending venues', () => {
-
-        let queryParams          = {near: 'Paris, France', limit: 3, radius: 2000}
-        let trendingVenues       = client.getTrendingVenues(queryParams)
-
-        return trendingVenues.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'venues')
-            assert.isArray(response.venues)
-            assert.include(response.geocode.where, 'paris france')
-        })
-    })
-
-    it('returns a 400 if no location parameter is passed', () => {
-
-        let queryParams          = {limit: 3, radius: 1700}
-        let trendingVenues       = client.getTrendingVenues(queryParams)
-
-        return trendingVenues.catch( (errorMsg) => {
-            assert.include(errorMsg, '400')
-            assert.include(errorMsg, 'Must provide parameter ll or userll or near')
-        })
-    })
-})
-
-describe('Test Nuddles.exploreVenues', () => {
-
-    it('returns one or more groups of recommendations', () => {
-
-        let params          = {near: 'Paris, France', limit: 3, radius: 1700}
-        let recommendations = client.exploreVenues(params)
-
-        return recommendations.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.equal(params.near, response.geocode.displayString)
-        })
-    })
-
-    it('returns a 400 if no location attribute is passed', () => {
-
-        let params          = {limit: 3}
-        let recommendations = client.exploreVenues(params)
-
-        return recommendations.catch( (errorMsg) => {
-            assert.include(errorMsg, 400)
-            assert.include(errorMsg,'Must provide parameters (ll and radius) or (sw and ne) or (near and radius) or (nearVenueId and ll) or (superVenueId) or (polygon)')
-
-        })
-
-    })
-
 })
 
 describe('Test Nuddles.suggestCompletion', () => {
@@ -309,53 +196,5 @@ describe('Test Nuddles.searchSpecials', () => {
             assert.include(errorMsg, 'Must provide parameter ll')
         })
     })
-})
-
-describe('Test Nuddles.searchEvents', () => {
-
-
-    it('returns a list of events matching the search params', () => {
-        let params   = {domain: 'songkick.com', eventId: '8183976'}
-        let events = client.searchEvents(params)
-
-        return events.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'events')
-        })
-    })
-
-    it('returns a 400 bad request if no domain is passed', () => {
-        let params   = {eventId: '8183976'}
-        let events = client.searchEvents(params)
-        return events.catch( (errorMsg) => {
-            assert.include(errorMsg, '400')
-            assert.include(errorMsg, 'Must provide parameter domain')
-        })
-    })
-
-    it('returns a 400 bad request if no eventId is passed', () => {
-        let params   = {domain: 'songkick.com'}
-        let events = client.searchEvents(params)
-        return events.catch( (errorMsg) => {
-            assert.include(errorMsg, '400')
-            assert.include(errorMsg, 'Must specify eventId or participantId')
-        })
-    })
-
-})
-
-describe('Test Nuddles.getEventCategories', () => {
-
-    it('return a list of event categories', () => {
-        let eventCategories = client.getEventCategories()
-
-        return eventCategories.then( (data) => {
-            let response = data.response
-            assert.equal(200, data.meta.code)
-            assert.property(response, 'categories')
-        })
-    })
-
 })
 
--- tests/userless/venue.js
+++ tests/userless/venue.js
@@ -3,19 +3,107 @@ const assert        = chai.assert
 const Venue         = require('../../index').Venue
 let client          = require('../config').userlessClient
 
-const clintonBaking = new Venue(client, "40a55d80f964a52020f31ee3")
+const venue         = new Venue(client, "40a55d80f964a52020f31ee3")
+
+describe('Test Venue.searchVenues', () => {
+
+    it('searches venues and returns results based on location',() => {
+
+        let params          = {'near': 'Paris, France'}
+        let searchVenues    =  venue.search(params)
+        return searchVenues.then( (data) => {
+            let response = data.response
+            assert.equal(200, data.meta.code)
+            assert.property(response, 'venues')
+            assert.isArray(response.venues)
+            assert.include(response.geocode.where, 'paris france')
+        })
+    })
+})
+
+describe('Test Nuddles.exploreVenues', () => {
+
+    it('returns one or more groups of recommendations', () => {
+
+        let params          = {near: 'Paris, France', limit: 3, radius: 1700}
+        let recommendations = venue.explore(params)
+
+        return recommendations.then( (data) => {
+            let response = data.response
+            assert.equal(200, data.meta.code)
+            assert.equal(params.near, response.geocode.displayString)
+        })
+    })
+
+    it('returns a 400 if no location attribute is passed', () => {
+
+        let params          = {limit: 3}
+        let recommendations = venue.explore(params)
+
+        return recommendations.catch( (errorMsg) => {
+            assert.include(errorMsg, 400)
+            assert.include(errorMsg,'Must provide parameters (ll and radius) or (sw and ne) or (near and radius) or (nearVenueId and ll) or (superVenueId) or (polygon)')
+
+        })
+
+    })
+
+})
+
+describe(' Test Nuddles.getVenueCategories', (done) => {
+
+    it('returns a list of all categories', () => {
+        let venueCategories = venue.getCategories()
+        return venueCategories.then( (data) => {
+            let response = data.response
+            assert.equal(200, data.meta.code)
+            assert.property(response, 'categories')
+            assert.isArray(response.categories)
+            assert.isAtLeast(response.categories.length, 1)
+        })
+    })
+
+})
+
+describe('Test Nuddles.getTrendingVenues', () => {
+
+    it('returns a list of trending venues', () => {
+
+        let queryParams          = {near: 'Paris, France', limit: 3, radius: 2000}
+        let trendingVenues       = venue.getTrending(queryParams)
+
+        return trendingVenues.then( (data) => {
+            let response = data.response
+            assert.equal(200, data.meta.code)
+            assert.property(response, 'venues')
+            assert.isArray(response.venues)
+            assert.include(response.geocode.where, 'paris france')
+        })
+    })
+
+    it('returns a 400 if no location parameter is passed', () => {
+
+        let queryParams          = {limit: 3, radius: 1700}
+        let trendingVenues       = venue.getTrending(queryParams)
+
+        return trendingVenues.catch( (errorMsg) => {
+            assert.include(errorMsg, '400')
+            assert.include(errorMsg, 'Must provide parameter ll or userll or near')
+        })
+    })
+})
 
 describe('Test Venue.getDetails()', () => {
 
     it('will return details about a venue based on id', () => {
 
-        let venueDetails = clintonBaking.getDetails()
+        let venueDetails = venue.getDetails()
 
         return venueDetails.then( (data) => {
             let response = data.response
             assert.equal(200, data.meta.code)
             assert.property(response, 'venue')
-            assert.equal(clintonBaking.venueId, response.venue.id)
+            assert.equal(venue.venueId, response.venue.id)
         })
     })
 
@@ -34,7 +122,7 @@ describe('Test Venue.getPhotos', () => {
 
     it('returns a list of photos', () => {
 
-        let photosList = clintonBaking.getPhotos()
+        let photosList = venue.getPhotos()
         return photosList.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'photos')
@@ -46,7 +134,7 @@ describe('Test Venue.getEvents', () => {
 
     it('returns a list of events for that venue', () => {
 
-        let photosList = clintonBaking.getEvents()
+        let photosList = venue.getEvents()
         return photosList.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'events')
@@ -58,7 +146,7 @@ describe('Test Venue.getLikes', () => {
 
     it('returns the number of likes for that venue', () => {
 
-        let likes = clintonBaking.getLikes()
+        let likes = venue.getLikes()
         return likes.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'likes')
@@ -70,7 +158,7 @@ describe('Test Venue.getNextVenues', () => {
 
     it('returns a list of the most visited venues after this one', () => {
 
-        let nextVenues = clintonBaking.getNextVenues()
+        let nextVenues = venue.getNextVenues()
         return nextVenues.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'nextVenues')
@@ -82,7 +170,7 @@ describe('Test Venue.getOpeningHours', () => {
 
     it('returns opening hours for a venue', () => {
 
-        let openingHours = clintonBaking.getOpeningHours()
+        let openingHours = venue.getOpeningHours()
         return openingHours.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'hours')
@@ -95,7 +183,7 @@ describe('Test Venue.getThirdPartyLinks', () => {
 
     it('returns third party links for a venue', () => {
 
-        let links = clintonBaking.getThirdPartyLinks()
+        let links = venue.getThirdPartyLinks()
         return links.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'links')
@@ -107,7 +195,7 @@ describe('Test Venue.getMenu', () => {
 
     it('returns a menu for a venue', () => {
 
-        let menu = clintonBaking.getMenu()
+        let menu = venue.getMenu()
         return menu.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'menu')
@@ -118,7 +206,7 @@ describe('Test Venue.getMenu', () => {
 describe('Test Venue.getTips', () => {
 
     it('returns tips for a venue', () => {
-        let tips = clintonBaking.getTips()
+        let tips = venue.getTips()
         return tips.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'tips')
@@ -129,7 +217,7 @@ describe('Test Venue.getTips', () => {
 describe('Test Venue.getLists', () => {
 
     it('returns a list of lists the venue is part of', () => {
-        let lists = clintonBaking.getLists()
+        let lists = venue.getLists()
         return lists.then( (data) => {
             assert.equal(200, data.meta.code)
             assert.property(data.response, 'lists')
